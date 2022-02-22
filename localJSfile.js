7173a144663b1857fccbc16fa5fc583585047cea
barrieu <37584427+barrieu@users.noreply.github.com>
2022-02-22 10:38:12 +0000

    put js into external file

---
+++ localJSfile.js
@@ -0,0 +1,28 @@
+function initialStuff(){
+    const jsPsych = initJsPsych();
+
+    var timeline = [];
+
+    const hello_trial = {
+      type: jsPsychHtmlKeyboardResponse,
+      stimulus: 'Hello world!'
+    };
+    timeline.push(hello_trial);
+
+    const goodbye_trial = {
+      type: jsPsychHtmlKeyboardResponse,
+      stimulus: 'goodbye world!'
+    };
+    timeline.push(goodbye_trial);
+
+    function consoleOutput(){
+      var version = jsPsych.version();
+      console.log(version);
+    };
+
+    var sayItOut = {
+         type: jsPsychCallFunction,
+         func: consoleOutput
+     };
+     timeline.push(sayItOut);
+}
